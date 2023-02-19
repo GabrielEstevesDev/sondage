cca35ba34bdc5e698f764b3d307d32a2de75cce3
GabrielEstevesDev <112899019+GabrielEstevesDev@users.noreply.github.com>
2023-02-19 19:42:43 +0100

    inscription et connexion
    nouvelles fonctionnalités
    respect de l'architecture MVC
    css amélioré

--- controle/inscription.php
+++ controle/inscription.php
@@ -1,25 +1,26 @@
 <?php
 function inscription(){
-
 /*if($id == NULL) {
 	header("Location: ../ident.php");
 }*/
-
-	$login =  isset($_POST['silogin'])?($_POST['silogin']):'';
-	$email =  isset($_POST['siemail'])?($_POST['siemail']):'';
-	$mdp =  isset($_POST['simdp'])?($_POST['simdp']):'';
-
-	
-	if(identExists($login, $email)) {
-		$msgAcc = "Le login et/ou le mail existe déjà";
-		$_SESSION['msgAcc'] = $msgAcc;
-		require('./vue/utilisateur/ident.tpl');
-	} else {
-		signIn($login,$email,$mdp);
-		$msgAcc = "Compte créé, vous pouvez vous connecter";
-		$_SESSION['msgAcc'] = $msgAcc;
-		require('./vue/utilisateur/ident.tpl');
+	if(count($_POST)==0) {
+		require ("./vue/inscription/inscription.tpl");
 	}
+	else {
+		$login =  isset($_POST['silogin'])?($_POST['silogin']):'';
+		$email =  isset($_POST['siemail'])?($_POST['siemail']):'';
+		$mdp =  isset($_POST['simdp'])?($_POST['simdp']):'';
+		if(identExists($login, $email)) {
+			$msgAcc = "Le login et/ou le mail existe déjà";
+			$_SESSION['msgAcc'] = $msgAcc;
+			require('./vue/inscription/inscription.tpl');
+		} else {
+			signIn($login,$email,$mdp);
+			$url = "./index.php?controle=connexion&action=ident";
+			header("Location:" .$url);
+		}
+	}
+	
 	//var_dump($login );
 	//var_dump($email);
 	// IMPORTANT Rappel PDO
@@ -31,7 +32,7 @@ function identExists($login, $email) {
 	//var_dump(verifLogin($login));
 	//var_dump(verifEmail($email));
 	require('./modele/utilisateurBD.php');
-	if(verifLogin($login) /*|| verifEmail($email)*/) {
+	if(verifLogin($login) || verifEmail($email)) {
 		return true;
 	} else {
 		return false;
--- vue/navbar/navbar.js
+++ vue/navbar/navbar.js
@@ -45,7 +45,7 @@ var page;
 var body;
 var toogle;
 var header;
-
+var htlmLiUser;
 window.addEventListener("load", init);
 function init() {
   toogle = document.querySelector(".toogle");
@@ -56,6 +56,12 @@ function init() {
   window.addEventListener("scroll", navPosition);
   window.addEventListener("resize", navPosition);
   toogle.addEventListener("click", navResp);
+  var liConnected = document.getElementById("connected");
+  if (liConnected != null) {
+    htlmLiUser = liConnected.innerHTML;
+    liConnected.addEventListener("mouseenter", OverLiUser);
+    liConnected.addEventListener("mouseleave", OutLiUser);
+  }
 }
 
 function navPosition() {
@@ -72,4 +78,13 @@ function navPosition() {
 
 function navResp() {
   body.classList.toggle("open");
+}
+
+function OverLiUser(e) {
+  e.target.innerHTML =
+    "<a href='./index.php?controle=logout&action=deconnexion'>Déconnexion</a>";
+}
+
+function OutLiUser(e) {
+  e.target.innerHTML = htlmLiUser;
 }
